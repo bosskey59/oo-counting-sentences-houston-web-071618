1a473fa9033a03a80637747215640ef2222bab03
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-07-19 16:53:28 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    char_array=self.split(" ")
+    counter =0
+    char_array.each do |char|
+      if char.end_with?("!") || char.end_with?("?") || char.end_with?(".")
+        counter+=1
+      end
+    end
+    counter
   end
 end
